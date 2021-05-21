cd8d896f54d12c25ee48f1ace9d8267330853184
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-21 20:22:29 +0900

    fix

--- back/internal/controller/home.go
+++ back/internal/controller/home.go
@@ -2,25 +2,26 @@ package controller
 
 import (
 	"k42un0k0blog/pkg/model"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type HomeController struct{
+type HomeController struct {
 	blogRepository model.BlogRepository
 }
 
-func InitHomeController(blogRepository model.BlogRepository) HomeController{
-	h:= HomeController{}
+func InitHomeController(blogRepository model.BlogRepository) HomeController {
+	h := HomeController{}
 	h.blogRepository = blogRepository
-	return h;
+	return h
 }
 
-func (homeController HomeController) HomeGet(c *gin.Context){
-	b, err:=homeController.blogRepository.FindById(1)
-	if err != nil{
-		c.JSON(502,err)
-	}else{
-		c.JSON(200,b)
+func (homeController HomeController) HomeGet(c *gin.Context) {
+	b, err := homeController.blogRepository.FindById(1)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+	} else {
+		c.JSON(http.StatusOK, b)
 	}
 }
