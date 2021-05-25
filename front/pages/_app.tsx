71d9f220f8b51a7e7fea4a5ef49982677be93c9f
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-26 04:13:41 +0900

    use react-query provider

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '../styles/globals.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 type Props = {
   Component: React.VFC;
   pageProps: Record<string, unknown>;
 };
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: Props): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
