90f928b4f85fa38f86362dda5122741e16406a4b
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-26 18:37:21 +0900

    update request test

--- back/cmd/main_test.go
+++ back/cmd/main_test.go
@@ -1,43 +1,48 @@
 package main_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"k42un0k0blog/internal"
 	"k42un0k0blog/internal/test"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// type JSON map[string]interface{}
+type JSON map[string]interface{}
 
-// func jsonToBody(j JSON) io.Reader {
-// 	b, _ := json.Marshal(j)
-// 	return bytes.NewReader(b)
-// }
+func jsonToBody(j JSON) io.Reader {
+	b, _ := json.Marshal(j)
+	return bytes.NewReader(b)
+}
 
-func TestUserRequest(t *testing.T) {
+func TestSignIn(t *testing.T) {
 
 	ts := httptest.NewServer(internal.ConfigServer())
 	defer ts.Close()
 	u, _ := url.Parse(ts.URL)
 
-	userCreate := test.TargetSchemeValue{
-		StatusCode: 401,
-		Method:     http.MethodPost,
-		Scheme:     u.Scheme,
-		Host:       u.Host,
-		Path:       "/users",
-		Body:       nil,
+	request, e := test.NewRequest(u, http.MethodPost, "/sign_in", jsonToBody(JSON{"username": "test@test.com", "password": "password"}))
+	if e != nil {
+		t.Fatal(e)
 	}
-	userUpdate := test.TargetSchemeValue{
-		StatusCode: 401,
-		Method:     http.MethodPut,
-		Scheme:     u.Scheme,
-		Host:       u.Host,
-		Path:       "/users/1",
-		Body:       nil,
+	request.Header.Set("Content-Type", "application/json; charset=utf-8")
+	requestInput, e := test.ValidateRequestInput(request, u.Scheme, u.Host)
+	if e != nil {
+		t.Fatal(e)
 	}
-	test.ValidateScheme(t, userCreate)
-	test.ValidateScheme(t, userUpdate)
+	response, body, e := test.DoAndGetBody(request)
+	if e != nil {
+		t.Fatal(e)
+	}
+	assert.Equal(t, 200, response.StatusCode)
+	e = test.ValidateResponseInput(requestInput, response, body)
+	if e != nil {
+		t.Fatal(e)
+	}
 }
--- back/internal/test/request.go
+++ back/internal/test/request.go
@@ -2,93 +2,70 @@ package test
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"testing"
+	"net/url"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	legacyrouter "github.com/getkin/kin-openapi/routers/legacy"
-	"github.com/stretchr/testify/assert"
 )
 
-type TargetSchemeValue struct {
-	StatusCode int
-	Method     string
-	Scheme     string
-	Host       string
-	Path       string
-	Body       io.Reader
-}
-
-func ValidateScheme(t *testing.T, value TargetSchemeValue) error {
-	request, e := http.NewRequest(value.Method, value.Path, value.Body)
-	if e != nil {
-		return e
-	}
-
-	// test用のサーバーに合わせてスキーマとホストをセット
-	request.URL.Scheme = value.Scheme
-	request.URL.Host = value.Host
-
-	// requestのvalidation
-	requestValidationInput := createRequestValidationInput(request, value.Scheme, value.Host)
-
-	ctx := context.Background()
-	if err := openapi3filter.ValidateRequest(ctx, requestValidationInput); err != nil {
-		return err
-	}
-
+func DoAndGetBody(request *http.Request) (*http.Response, []byte, error) {
 	// responseの取得とresponse.Bodyの取得
 	response, e := http.DefaultClient.Do(request)
 	if e != nil {
-		return e
+		return nil, nil, e
 	}
 	defer response.Body.Close()
 
 	body, e := ioutil.ReadAll(response.Body)
 	if e != nil {
-		return e
+		return nil, nil, e
+	}
+	return response, body, nil
+}
+func NewRequest(u *url.URL, method string, path string, body io.Reader) (*http.Request, error) {
+	request, e := http.NewRequest(method, path, body)
+	if e != nil {
+		return nil, e
 	}
 
-	// responseのvalidation
-	assert.Equal(t, value.StatusCode, response.StatusCode)
-
-	responseValidationInput := createResponseValidationInput(requestValidationInput, response, body)
-
-	return openapi3filter.ValidateResponse(ctx, responseValidationInput)
+	request.URL.Scheme = u.Scheme
+	request.URL.Host = u.Host
+	return request, nil
 }
 
-func createRequestValidationInput(request *http.Request, scheme string, host string) *openapi3filter.RequestValidationInput {
+func ValidateRequestInput(request *http.Request, scheme string, host string) (*openapi3filter.RequestValidationInput, error) {
 	ctx := context.Background()
 	loader := &openapi3.Loader{Context: ctx}
 	doc, e := loader.LoadFromFile("/app/api/openapi.yml")
 	if e != nil {
-		fmt.Print(e)
+		return nil, e
 	}
 	doc.Validate(ctx)
 	router, _ := legacyrouter.NewRouter(doc)
 
 	route, pathParams, e := router.FindRoute(request)
 	if e != nil {
-		fmt.Print(e)
+		return nil, e
 	}
 	requestValidationInput := &openapi3filter.RequestValidationInput{
 		Request:    request,
 		PathParams: pathParams,
 		Route:      route,
 	}
-	return requestValidationInput
+	return requestValidationInput, openapi3filter.ValidateRequest(ctx, requestValidationInput)
 }
 
-func createResponseValidationInput(requestValidationInput *openapi3filter.RequestValidationInput, response *http.Response, body []byte) *openapi3filter.ResponseValidationInput {
+func ValidateResponseInput(requestValidationInput *openapi3filter.RequestValidationInput, response *http.Response, body []byte) error {
+	ctx := context.Background()
 	responseValidationInput := &openapi3filter.ResponseValidationInput{
 		RequestValidationInput: requestValidationInput,
 		Status:                 response.StatusCode,
 		Header:                 response.Header,
 	}
 	responseValidationInput.SetBodyBytes(body)
-	return responseValidationInput
+	return openapi3filter.ValidateResponse(ctx, responseValidationInput)
 }
