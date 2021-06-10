bebe12f17bbf917a9df4c5160d118106318efe8d
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-10 20:05:11 +0900

    fix config

--- front/jest.config.ts
+++ front/jest.config.ts
@@ -172,9 +172,6 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['\\\\node_modules\\\\', '\\.pnp\\.[^\\\\]+$'],
