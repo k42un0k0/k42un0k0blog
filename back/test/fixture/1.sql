29b0c80f2196109ea367a57980b235e6e75dc22c
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-15 07:21:56 +0900

    add seed

---
+++ back/test/fixture/1.sql
@@ -0,0 +1,18 @@
+INSERT INTO `blog_daos` (
+        `created_at`,
+        `updated_at`,
+        `deleted_at`,
+        `title`,
+        `body`,
+        `blog_type`,
+        `published_at`
+    )
+VALUES (
+        '2021-05-22 22:49:01.532',
+        '2021-05-22 22:49:01.532',
+        NULL,
+        'hello',
+        '$$c=\sqrt\pi$$',
+        0,
+        NULL
+    );
--- scripts/db.sh
+++ scripts/db.sh
@@ -10,6 +10,14 @@ function migrate(){
     docker-compose run --rm app migrate -path ./migrations -database "mysql://root:example@tcp(db:3306)/k42un0k0blog_develop" $@
 }
 
+function seed(){
+    for FILE in $@
+    do
+        echo "exec $FILE" 
+        docker-compose exec -T db mysql -uroot -pexample k42un0k0blog_develop < $FILE
+    done
+}
+
 if [ $# -gt 0 ]; then
     if [ $1 = "--help" ]; then
         cat<<EOF
@@ -23,11 +31,15 @@ Commands:
     migrate [up | down | force] [arg...]
         migrateコマンドをラップしたもの
         詳細は"docker-compose run --rm app migrate --help"を参照
+    seed FILE...
+        指定されたファイルをmysqlに取り込む
 EOF
     elif [ $1 = "create_migration" ]; then
         create_migration ${@:2}
     elif [ $1 = "migrate" ]; then
         migrate ${@:2}
+    elif [ $1 = "seed" ]; then
+        seed ${@:2}
     fi
 else
     $0 --help
