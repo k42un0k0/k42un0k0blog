d48d4d179cf106bfa4317aad9ab9805cfdc499ac
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-15 05:48:34 +0900

    union to tuple

--- front/@types/utils.d.ts
+++ front/@types/utils.d.ts
@@ -6,3 +6,11 @@ declare module 'markdown-it-texmath' {
   const d: any;
   export default d;
 }
+
+type UnionToTuple<T> = UnionPop<T> extends never ? [] : [...UnionToTuple<Exclude<T, UnionPop<T>>>, UnionPop<T>];
+
+type UnionToOverloadFunction<T> = (T extends any ? (a: (b: T) => void) => void : never) extends (c: infer U) => void
+  ? U
+  : never;
+
+type UnionPop<T> = UnionToOverloadFunction<T> extends (a: infer U) => void ? U : never;
--- front/apiModel/blog.ts
+++ front/apiModel/blog.ts
@@ -3,5 +3,6 @@ import { MonoidAny } from 'fp-ts/boolean';
 import type { BlogType } from './../api/@types/index';
 
 export function isBlogType(a: number): a is BlogType {
-  return array.foldMap(MonoidAny)((b: BlogType) => b === a)([0, 1, 2]);
+  const types: UnionToTuple<BlogType> = [0, 1, 2];
+  return array.foldMap(MonoidAny)((b: BlogType) => b === a)(types);
 }
